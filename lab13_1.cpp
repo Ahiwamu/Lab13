1934e6bb4a6dbb4042995fb676041ad97c91a502
Ahiwamu <niceniceiloveyou@gmail.com>
2024-12-27 14:48:50 +0700

    finished

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,31 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double a[], int n, double b[]){
+    double summean = 0.0, mean;
+    double sumsd = 0.0, sd;
+    double multi = 1.0, geo;
+    double harmo = 0.0, hm;
+    double maxx = -9999999;
+    double minn = 9999999;
+    for(int i=0; i<n; i++){
+        summean += a[i];
+        sumsd += a[i]*a[i];
+        multi *= a[i];
+        harmo += 1.0/a[i];
+        if(maxx < a[i]) maxx = a[i];
+        if(minn > a[i]) minn = a[i];
+    }
+    mean = summean / n;
+    sd = sqrt((sumsd/n-mean*mean) );
+    geo = pow(multi, 1.0 / n);
+    hm = n / harmo;
+
+    b[0] = mean;
+    b[1] = sd;
+    b[2] = geo;
+    b[3] = hm;
+    b[4] = maxx;
+    b[5] = minn; 
+}
